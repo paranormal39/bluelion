49df89b1880519788df5416dd777618da7b994d1
Anthony Montanez <allnightergamestx@gmail.com>
2019-03-15 19:21:54 -0500

    fixed app issue

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
-import home from './pages/home';
+import home from './pages/home.jsx';
 //import navbar from './Components/Navbar.jsx';
 
 class App extends Component {
