48f58e6118c0fed9c4afb19bc111a8e7fe9b4f31
Farooq AR <farooqbckk@gmail.com>
2019-03-19 11:05:03 +0500

    fix route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import home from './pages/home.jsx';
 //import navbar from './Components/Navbar.jsx';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
       <Router>
-        <div>
-          <Route exact path = "/" component={home}/>
-        </div>
+        <div className="App">
+          <div>
+            <Route path="/" component={home} />
+          </div>
 
+        </div>
       </Router>
-      </div>
     );
   }
 }
